8970fb0d30a434dd8d56b89a52b65fdd80c1e1d2
saranya <saranyasanjana09@gmail.com>
2025-11-26 04:32:36 +0530

    saranya-added-string

---
+++ html-file/javascript/string.js
@@ -0,0 +1,11 @@
+var firstName = "venkatesh";
+var lastName = "saranya";
+
+console.log (firstName.length, lastName.length);
+console.log(firstName.slice(3));
+console.log(firstName.slice(-2));
+console.log(lastName.substring(2,5));
+console.log(lastName.substr(2,5));
+console.log(lastName.replace("a", "v"));
+console.log(lastName.toUpperCase());
+console.log(lastName.replaceAll("a", "v"));
