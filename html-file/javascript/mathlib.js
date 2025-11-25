31410a3f5fd31e912a34c1ea682724b77937c362
saranya <saranyasanjana09@gmail.com>
2025-11-26 04:07:53 +0530

    saranya-added-math-library

---
+++ html-file/javascript/mathlib.js
@@ -0,0 +1,17 @@
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.SQRT2);
+console.log(Math.SQRT_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG10E);
+console.log(Math.round(3.6));
+
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.4));
+console.log(Math.trunc(4.4));
+console.log(Math.sign(4.4));
+console.log(Math.pow(16, 3));
+console.log(Math.abs(-4.4));
+console.log(Math.max(10,678,349,289,1000));
+console.log(Math.min(839,765,89,34,728));
