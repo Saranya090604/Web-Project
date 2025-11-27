815af800ed614f018000b70b1149ca6c9da2cff1
saranya <saranyasanjana09@gmail.com>
2025-11-27 11:34:30 +0530

    saranya-added-movies

--- html-file/javascript/arrays.js
+++ html-file/javascript/arrays.js
@@ -1,10 +1,25 @@
 
 
-const bikeBrands = ["BMW", "AUDI", "JAGQUAR"];
-bikeBrands.forEach ((item, index) => {
-    if (item === "jaguar") {
-        bikeBrands[index] = "Honda";
-    }
-});
-console.log(bikeBrands);
+//const bikeBrands = ["BMW", "AUDI", "JAGQUAR"];
+//bikeBrands.forEach ((item, index) => {
+   // if (item === "jaguar") {
+        //bikeBrands[index] = "Honda";
+    //}
+//});
+//console.log(bikeBrands);
+
+//const movies = new Array(10);
+//movies[0] = "";
+//movies[1] = "";
+//movies[2] = "";
+//movies[3] = "";
+//movies[4] = "";
+//movies[5] = "";
+//movies[6] = "";
+movies[7] = "";
+movies[8] = "";
+movies[9] = "";
+console.log("movies");
+
+
 
