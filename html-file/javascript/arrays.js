4f742c4ac61a4a0b7e1004fdc42108a103f947f3
saranya <saranyasanjana09@gmail.com>
2025-11-27 12:18:14 +0530

    saranya-added-slice

--- html-file/javascript/arrays.js
+++ html-file/javascript/arrays.js
@@ -44,5 +44,7 @@
 //console.log(num1 + num2);
 
 const numbers = [1, 2, 3, 4, 5];
-numbers.splice(3, 2, 6, 7, 8);
+//numbers.splice(3, 2, 6, 7, 8);
+//numbers.splice(1, 1);
+const z = numbers.slice(4);
 console.log(numbers);
