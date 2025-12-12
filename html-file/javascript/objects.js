92bfd2eff9cd7871a4d4b47ff53472b16f88aba4
saranya <saranyasanjana09@gmail.com>
2025-12-12 09:08:38 +0530

    object-file-added

---
+++ html-file/javascript/objects.js
@@ -0,0 +1,6 @@
+const person = {};
+person ["firstname"] = "venkat";
+person ["lastname"] = "saranya";
+person.age = 22;
+person.designation = "java developer";
+console.log(person.firstname + "works as a" + person.designation);
