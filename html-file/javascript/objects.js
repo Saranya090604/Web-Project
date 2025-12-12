0afe4daa323ea26022dd69ecd3d957db15b72c96
saranya <saranyasanjana09@gmail.com>
2025-12-12 09:11:31 +0530

    object-address-changed

--- html-file/javascript/objects.js
+++ html-file/javascript/objects.js
@@ -3,4 +3,5 @@ person ["firstname"] = "venkat";
 person ["lastname"] = "saranya";
 person.age = 22;
 person.designation = "java developer";
-console.log(person.firstname + "works as a" + person.designation);
+person.adderess = "pallavaram";
+console.log(person);
