13e6e43a7a1e988ae20f0c87f253cd37ba17df80
saranya <saranyasanjana09@gmail.com>
2025-12-12 09:17:11 +0530

    object-new-added

--- html-file/javascript/objects.js
+++ html-file/javascript/objects.js
@@ -1,7 +1,10 @@
-const person = {};
-person ["firstname"] = "venkat";
-person ["lastname"] = "saranya";
-person.age = 22;
-person.designation = "java developer";
-person.adderess = "pallavaram";
-console.log(person);
+//const person = {};
+//person ["firstname"] = "venkat";
+//person ["lastname"] = "saranya";
+//person.age = 22;
+//person.designation = "java developer";
+//person.adderess = "pallavaram";
+//console.log(person);
+
+const school = new object();
+console.log(school);
