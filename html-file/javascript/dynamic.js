e51df87069478ab8dcca2b51012288bf6540ba9e
saranya <saranyasanjana09@gmail.com>
2025-12-13 16:48:08 +0530

    dynamic-array-added

---
+++ html-file/javascript/dynamic.js
@@ -0,0 +1,7 @@
+const h1 = document .querySelector("h1");
+h1.innerText = "Hello JavaScript";
+const customDiv = document.getElementById("customDiv");
+//customDiv.innerText = "Lets Learn Javascript";  
+const h2 = document.createElement("h2");
+h2.textcontent = "Hello, How are you?";
+console.log(h2);
