773a9c33c54de36abd3be750d2dbf9b4f3c14ce8
saranya <saranyasanjana09@gmail.com>
2025-12-13 17:14:18 +0530

    dynamic-array-example-added

--- html-file/javascript/dynamic.js
+++ html-file/javascript/dynamic.js
@@ -1,7 +1,16 @@
-const h1 = document .querySelector("h1");
-h1.innerText = "Hello JavaScript";
-const customDiv = document.getElementById("customDiv");
-//customDiv.innerText = "Lets Learn Javascript";  
-const h2 = document.createElement("h2");
-h2.textcontent = "Hello, How are you?";
-console.log(h2);
+const div = document.querySelector("div");
+const input = document.createElement("input");
+input.value = "Angular Course";
+input.type = "text";
+input.placeholder = "Enter some content";
+input.id = "course_field";
+const h1 = document.createElement("h1");
+h1.innerText = "Dynamic conent in HTML";
+const button = document.createElement("button");
+button.textcontent = "Click Me";
+div.appendChild(h1);
+div.appendChild(input);
+div.appendChild(button);
+button.addEventListener("click", function () {
+    window.alert("Hey, This is a button");
+});
