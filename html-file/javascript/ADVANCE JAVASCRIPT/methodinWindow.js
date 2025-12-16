ff573c2d322d3cdcd4b3365d70956a6133059dec
saranya <saranyasanjana09@gmail.com>
2025-12-16 22:28:07 +0530

    methodinWindow-file-added

---
+++ html-file/javascript/ADVANCE JAVASCRIPT/methodinWindow.js
@@ -0,0 +1,9 @@
+//window.alert("Welcome to Javascript");
+//const age = window.prompt("Please Enter your Age");
+//if (parseInt(age) !== Nan) {
+    //window.alert("valid Age: " + age);
+//} else {
+   // window.alert("please input correct Age");
+//}
+
+console.log(window.navigator.geolocation);
