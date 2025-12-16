6199f6500a44f1c180093f381a83d2736d500b0f
saranya <saranyasanjana09@gmail.com>
2025-12-16 22:09:19 +0530

    domGetter-file-added

---
+++ html-file/javascript/ADVANCE JAVASCRIPT/domGetter.js
@@ -0,0 +1,8 @@
+//getElementId
+const divRoot = window.document.getElementById("root");
+console.log(divRoot);
+//querySelector
+const h1 = window.document.querySelector("h1");
+console.log(h1);
+const allH1 = window.document.querySelectorAll("h1");
+console.log(allH1);
